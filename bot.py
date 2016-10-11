62631240eec9af3231621e8b04a778e00f14db75
Kazauwa <i.heart.d.dead@gmail.com>
2016-10-11 20:16:48 +0300

    Human and csv logger, newyear countdown

--- bot.py
+++ bot.py
@@ -4,10 +4,30 @@ import os
 import operator
 import ephem
 import json
-from datetime import date
+import csv
+from datetime import datetime, date
 
 
 bot = telebot.TeleBot(os.environ.get("TELETOKEN"))
+
+
+def writelog(message):
+    timestamp = datetime.fromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S')
+    line = '{0} {1}: \'{2}\'\n'.format(timestamp, message.from_user.username, message.text)
+    with open('log', 'a') as logger:
+        logger.write(line)
+
+
+def writecsv(message):
+    date = datetime.fromtimestamp(message.date).strftime('%Y-%m-%d')
+    time = datetime.fromtimestamp(message.date).strftime('%H:%M:%S')
+    log = [{'date': date, 'time': time, 'username': message.from_user.username, 'content': message.text}]
+    with open('log.csv', 'a', encoding='utf-8') as logger:
+        fields = [field for field in log[0].keys()]
+        writer = csv.DictWriter(logger, fields, delimiter=';')
+        if not os.path.isfile('log.csv'):
+            writer.writeheader()
+        writer.writerow(log[0])
 
 
 def init_phrasebook():
@@ -40,7 +60,10 @@ def words_to_digits(expression):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Init1')
+    writecsv(message)
+    reply = bot.reply_to(message, 'Init1')
+    writecsv(reply)
+    reply
 
 
 @bot.message_handler(commands=['count'])
@@ -81,7 +104,7 @@ def words_calculator(message):
 @bot.message_handler(func=lambda message: 'полнолуние' in message.text.lower())
 def next_newmoon(message):
     date = re.sub('[А-я]+\s+', '', message.text)
-   if not date:
+    if not date:
         bot.reply_to(message, 'Не забудьте написать дату!')
     reply = ephem.next_new_moon(date)
     bot.reply_to(message, reply)
@@ -99,7 +122,23 @@ def newyear_countdown(message):
     today = date.today()
     newyear = date(today.year + 1, 1, 1)
     result = newyear - today
-    bot.reply_to(message, "До нового {0} года осталось {1} дней.".format(newyear.year, result.days))
+    result = result.days
+    if result % 100 in range(5, 21) or result % 100 == 0:
+        days = 'дней'
+    if result % 10 in range(5, 9) or result % 10 == 0:
+        days = 'дней'
+    if result % 10 == 1:
+        days = 'день'
+    if result % 10 in range(2, 5):
+        days = 'дня'
+    bot.reply_to(message, "До нового {0} года осталось {1} {2}.".format(newyear.year, result, days))
+
+
+@bot.message_handler(func=lambda message: True)
+def true(message):
+    timestamp = datetime.fromtimestamp(message.date).strftime('%Y-%m-%d %H:%M:%S')
+    msg = bot.send_message(message.chat.id, timestamp)
+    bot.send_message(message.chat.id, msg)
 
 
 if __name__ == '__main__':
